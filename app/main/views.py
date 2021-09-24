a8ed8f35409095372f6ab64f8022e0a349c3f1bd
otienonick <nick.odongi@student.moringaschool.com>
2021-09-24 20:59:52 +0300

    added initial app files

---
+++ app/main/views.py
@@ -0,0 +1,5 @@
+from flask import render_template,request,redirect,url_for,abort
+from . import main
+
+
+#
---
+++ manage.py
@@ -0,0 +1,34 @@
+from app import create_app,db
+from  flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager,Server
+
+
+# creating app instance
+app = create_app('development')
+
+
+migrate = Migrate(app,db)
+manager = Manager(app)
+
+manager.add_command('server',Server)
+manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+
+def test():
+
+    """
+    Run the unit tests.
+
+    """
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db) 
+
+if __name__ == '__main__':
+    manager.run()
