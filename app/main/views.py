6a3eca02f9d649c42f8eff598e2ab83de48a8256
otienonick <nick.odongi@student.moringaschool.com>
2021-09-25 02:04:51 +0300

    added create_post function

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,10 @@
-from flask import render_template,request,redirect,url_for,abort
+from flask import render_template
 from . import main
+from ..requests import get_quote
 
 
-#
+
+@main.route("/")
+def home():
+    quotes = get_quote()
+    return render_template("index.html",  quotes=quotes)
