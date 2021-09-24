f420745d52c85be057a4a6c51501b49cb673672e
otienonick <nick.odongi@student.moringaschool.com>
2021-09-25 02:05:50 +0300

    added quotes class and its instance variables

---
+++ app/models.py
@@ -0,0 +1,13 @@
+class Quotes:
+
+    '''
+    Movie class to define Movie Objects
+
+    '''
+
+    def __init__(self,author,id,quote,permalink):
+        self.author = author
+        self.id = id
+        self.quote = quote
+        self.permalink = permalink
+   
