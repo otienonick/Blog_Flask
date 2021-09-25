341ebfbda04a3a6ac5c8c81e9ce0fd6e5500d5e5
otienonick <nick.odongi@student.moringaschool.com>
2021-09-25 03:42:43 +0300

    created User table

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+
 class Quotes:
 
     '''
@@ -10,4 +15,17 @@ class Quotes:
         self.id = id
         self.quote = quote
         self.permalink = permalink
+
+
+
+class User(db.Model,UserMixin):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer,primary_key = True)
+    email = db.Column(db.String(150),unique = True,index = True)
+    username= db.Column(db.String(150),index = True)
+    password =  db.Column(db.String(150))
+    date_created = db.Column(db.DateTime(timezone =True),default = func.now())
+    
+
+
    
