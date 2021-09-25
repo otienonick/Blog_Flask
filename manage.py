4d76036a1ae9841b12ee448549016c78081b0cd0
otienonick <nick.odongi@student.moringaschool.com>
2021-09-25 03:37:51 +0300

    added class user to shell

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 from app import create_app,db
 from  flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager,Server
+from app.models import User
 
 
 # creating app instance
@@ -28,7 +29,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db) 
+    return dict(app = app,db = db,User = User) 
 
 if __name__ == '__main__':
     manager.run()
