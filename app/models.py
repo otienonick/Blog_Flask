771bdc2adbc4da3e19d0ebdebb373f70d0b2f6ff
otienonick <nick.odongi@student.moringaschool.com>
2021-09-25 11:41:36 +0300

    modified signup and login page

--- app/models.py
+++ app/models.py
@@ -1,8 +1,11 @@
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
+from . import login_manager
 
-
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))  
 class Quotes:
 
     '''
