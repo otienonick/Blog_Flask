5ddb61cb8e979fa86d9b1b8e5c491dac941c7356
otienonick <nick.odongi@student.moringaschool.com>
2021-09-25 21:10:22 +0300

    modified dshell function by adding comment and likeclasses

--- app/models.py
+++ app/models.py
@@ -31,6 +31,8 @@ class User(db.Model,UserMixin):
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
     posts = db.relationship('Post',backref = 'user',passive_deletes = True)
+    comments = db.relationship('Comment', backref='user', passive_deletes=True)
+    likes = db.relationship('Like', backref='user', passive_deletes=True)
 
     
 class Post(db.Model):
@@ -39,5 +41,26 @@ class Post(db.Model):
     text = db.Column(db.String,nullable = False)
     date_created = db.Column(db.DateTime(timezone =True),default = func.now())
     author = db.Column(db.Integer,db.ForeignKey('user.id',ondelete = 'CASCADE'),nullable = False)
+    comments = db.relationship('Comment', backref='post', passive_deletes=True)
+    likes = db.relationship('Like', backref='post', passive_deletes=True)
+
+
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer,primary_key = True)
+    text = db.Column(db.String(200),nullable = False)
+    date_created = db.Column(db.DateTime(timezone =True),default = func.now())
+    author = db.Column(db.Integer,db.ForeignKey('user.id',ondelete = 'CASCADE'),nullable = False)
+    post_id = db.Column(db.Integer,db.ForeignKey('post.id',ondelete = 'CASCADE'),nullable = False)
+
+
+class Like(db.Model):
+    __tablename__ = 'likes'
+
+    id = db.Column(db.Integer,primary_key = True)
+    author = db.Column(db.Integer,db.ForeignKey('user.id',ondelete = 'CASCADE'),nullable = False)
+    post_id = db.Column(db.Integer,db.ForeignKey('post.id',ondelete = 'CASCADE'),nullable = False)
+
 
    
