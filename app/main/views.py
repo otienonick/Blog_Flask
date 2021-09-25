bb73ccd8080dec3a6c2a41fd7e83d175cd7fb555
otienonick <nick.odongi@student.moringaschool.com>
2021-09-25 19:04:19 +0300

    added create a post view function and its render html files

--- app/main/views.py
+++ app/main/views.py
@@ -1,22 +1,23 @@
-from flask import render_template,abort,redirect,url_for,request
+from flask import render_template,abort,redirect,url_for,request,flash
 from . import main
 from ..requests import get_quote
 from .forms import  UpdateProfile
 from .. import db,photos
-from ..models import User
-from flask_login import login_required
+from ..models import User,Post
+from flask_login import current_user
 
 
 @main.route("/")
 def index():
     quotes = get_quote()
-    return render_template("index.html",  quotes=quotes)
+    posts = Post.query.all()
+
+    return render_template("index.html",  quotes=quotes,user = current_user,posts = posts)
 
 
     
 
 @main.route('/user/<uname>')
-@login_required
 def profile(uname):
     user = User.query.filter_by(username = uname).first()
 
@@ -45,7 +46,6 @@ def update_profile(uname):
     return render_template('profile/update.html',form = form)
 
 @main.route('/user/<uname>/update/pic',methods = ['POST'])
-@login_required
 def update_pic(uname):
     user = User.query.filter_by(username = uname).first()
     if 'photo' in request.files:
@@ -54,6 +54,24 @@ def update_pic(uname):
         user.profile_pic_path = path
         db.session.commit()
     return redirect(url_for('main.profile',uname = uname))
+
+
+@main.route('/create_blog',methods = ['GET','POST'])
+def create_blog():
+    if request.method == 'POST':
+        text = request.form.get('text')
+        if not text:
+            flash('Blog section cannot be empty',category='error' )
+        elif len(text) < 5:
+            flash('Content  is too short', category='error')    
+        else:
+            post = Post(text = text,author =current_user.id)
+            db.session.add(post)
+            db.session.commit()
+            flash('Post created',category='success')  
+            return redirect(url_for('main.index'))  
+
+    return render_template('create_blog.html',user = current_user)        
 
     
 
--- app/models.py
+++ app/models.py
@@ -30,7 +30,14 @@ class User(db.Model,UserMixin):
     date_created = db.Column(db.DateTime(timezone =True),default = func.now())
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
-    
+    posts = db.relationship('Post',backref = 'user',passive_deletes = True)
 
+    
+class Post(db.Model):
+    __tablename__ = 'post'
+    id = db.Column(db.Integer,primary_key = True)
+    text = db.Column(db.String,nullable = False)
+    date_created = db.Column(db.DateTime(timezone =True),default = func.now())
+    author = db.Column(db.Integer,db.ForeignKey('user.id',ondelete = 'CASCADE'),nullable = False)
 
    
