a73990ae0d454bcd4c3cbe83a35faaed91965dde
otienonick <nick.odongi@student.moringaschool.com>
2021-09-25 21:15:14 +0300

    modified migrations files and added likes and comments collumns

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from app import create_app,db
 from  flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager,Server
-from app.models import User
+from app.models import User,Post,Comment,Like
 
 
 # creating app instance
@@ -29,7 +29,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User) 
+    return dict(app = app,db = db,User = User,Post = Post,Comment = Comment,Like = Like) 
 
 if __name__ == '__main__':
     manager.run()
