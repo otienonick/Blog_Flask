a8c6bc6bdbc12cc70710a8d521086b8e02b04302
otienonick <nick.odongi@student.moringaschool.com>
2021-09-26 22:35:10 +0300

    modified database

--- app/models.py
+++ app/models.py
@@ -20,7 +20,7 @@ class Quotes:
         self.permalink = permalink
 
 class User(db.Model,UserMixin):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     email = db.Column(db.String(150),unique = True,index = True)
     username= db.Column(db.String(150),index = True)
@@ -28,9 +28,9 @@ class User(db.Model,UserMixin):
     date_created = db.Column(db.DateTime(timezone =True),default = func.now())
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
-    posts = db.relationship('Post',backref = 'user',passive_deletes = True)
-    comments = db.relationship('Comment', backref='user', passive_deletes=True)
-    likes = db.relationship('Like', backref='user', passive_deletes=True)
+    posts = db.relationship('Post',backref = 'users',passive_deletes = True)
+    comments = db.relationship('Comment', backref='users', passive_deletes=True)
+    likes = db.relationship('Like', backref='users', passive_deletes=True)
 
     
 class Post(db.Model):
@@ -38,7 +38,7 @@ class Post(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     text = db.Column(db.String,nullable = False)
     date_created = db.Column(db.DateTime(timezone =True),default = func.now())
-    author = db.Column(db.Integer,db.ForeignKey('user.id',ondelete = 'CASCADE'),nullable = False)
+    author = db.Column(db.Integer,db.ForeignKey('users.id',ondelete = 'CASCADE'),nullable = False)
     comments = db.relationship('Comment', backref='post', passive_deletes=True)
     likes = db.relationship('Like', backref='post', passive_deletes=True)
 
@@ -49,7 +49,7 @@ class Comment(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     text = db.Column(db.String(200),nullable = False)
     date_created = db.Column(db.DateTime(timezone =True),default = func.now())
-    author = db.Column(db.Integer,db.ForeignKey('user.id',ondelete = 'CASCADE'),nullable = False)
+    author = db.Column(db.Integer,db.ForeignKey('users.id',ondelete = 'CASCADE'),nullable = False)
     post_id = db.Column(db.Integer,db.ForeignKey('post.id',ondelete = 'CASCADE'),nullable = False)
 
 
@@ -57,7 +57,7 @@ class Like(db.Model):
     __tablename__ = 'likes'
 
     id = db.Column(db.Integer,primary_key = True)
-    author = db.Column(db.Integer,db.ForeignKey('user.id',ondelete = 'CASCADE'),nullable = False)
+    author = db.Column(db.Integer,db.ForeignKey('users.id',ondelete = 'CASCADE'),nullable = False)
     post_id = db.Column(db.Integer,db.ForeignKey('post.id',ondelete = 'CASCADE'),nullable = False)
 
 
